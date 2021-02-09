49c531c8cbe5bb32911aa60c02b432da35611b8e
Marco897-Mantilla <74988132+Marco897-Mantilla@users.noreply.github.com>
2021-02-08 20:50:58 -0500

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 20:41:18 2021
+
+@author: marcomantilla
+"""
+
+x=input("Enter a number to count to:")
+x=int(x)
+y=1
+while y<=8x:
+    print(y)
+    y=y+1
